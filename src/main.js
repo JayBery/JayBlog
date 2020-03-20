4f63195e9331f9050b2f24e8a8a68b1e5464c6e8
yangjiabinhome <754626536@qq.com>
2020-03-20 16:01:49 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import { Button, Select } from 'element-ui';
+//将引入的组件注册为全局Vue组件
+Vue.component(Button.name, Button);
+Vue.component(Select.name, Select);
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   router,
   store,
   render: h => h(App)
