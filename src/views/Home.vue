986061425ebf3761e42a35de936f8b877f641abc
yangjiabinhome <754626536@qq.com>
2020-03-28 14:30:50 +0800

    首页推荐和最新板块铺设

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,6 +21,32 @@
         </div>
       </div>
     </div>
+    <div class="recommend">
+      <div>
+        <div class="title">推荐文章</div>
+          <el-carousel :interval="5000" indicator-position="none" type="card" arrow="always" class="d_jump" :height="bannerHeight + 'px'">
+          <el-carousel-item v-for="item in imgList" :key="item.id">
+            <img :src="item.idView" alt />
+            <div class="min_title">{{item.name}}</div>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+      <div>
+        <div class="title">最新文章</div>
+        <div class="card">
+          <img src="https://img.debuginn.cn/wp-content/uploads/2020/03/google-chrome-2020.jpeg" alt="">
+          <div class="text">程序员的插件推荐<br><span>Chrome 浏览器插件提高工作效率</span></div>
+        </div>
+        <div class="card">
+          <img src="https://img.debuginn.cn/wp-content/uploads/2020/03/google-chrome-2020.jpeg" alt="">
+          <div class="text">程序员的插件推荐<br><span>Chrome 浏览器插件提高工作效率</span></div>
+        </div>
+        <div class="card">
+          <img src="https://img.debuginn.cn/wp-content/uploads/2020/03/google-chrome-2020.jpeg" alt="">
+          <div class="text">程序员的插件推荐<br><span>Chrome 浏览器插件提高工作效率</span></div>
+        </div>
+      </div>
+    </div>
   </div>
     
 </template>
@@ -33,16 +59,60 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
-  }
+  },
+  data(){
+    return{
+     // 图片需要引入, 否则无法显示
+        imgList: [
+          {id: 0, name: '程序员chrome插件推荐', idView: require('../assets/images/banner3.jpg')},
+          {id: 1, name: 'vue引入框架UI的方法', idView: require('../assets/images/banner2.jpg')},
+          {id: 2, name: '数组倒序遍历方法', idView: require('../assets/images/banner1.jpg')}
+        ],
+        // 图片父容器高度
+        bannerHeight: 1000,
+        // 浏览器宽度
+        screenWidth: 0
+    }
+  },
+   methods: {
+    setSize: function() {
+      // 通过浏览器宽度(图片宽度)计算高度
+      this.bannerHeight = (426 / 1920) * this.screenWidth;
+    }
+  },
+  mounted() {
+    
+    // 首次加载时,需要调用一次
+    this.screenWidth = window.innerWidth;
+    this.setSize();
+    // 窗口大小发生改变时,调用一次
+    window.onresize = () => {
+      this.screenWidth = window.innerWidth;
+      this.setSize();
+    };
+  }
 }
 </script>
 
 <style scope>
 
+.min_title{
+  width: 85%;
+  color: rgb(255, 255, 255);
+  font-size: .056rem;
+  /* font-weight: bolder; */
+  text-align: left;
+  margin-top: .02rem;
+  margin-left: .03rem;
+  letter-spacing:.002rem;
+  overflow: hidden;
+  text-overflow:ellipsis;
+  white-space: nowrap;
+}
 
 .header
 {
-	background: #444 url(../images/banner.jpg);
+	background: #444 url(../assets/images/banner.jpg);
 	/* background-attachment: fixed;  背景滚动*/
 	background-repeat: no-repeat; 
 	background-size: cover;
@@ -108,6 +178,66 @@ export default {
 	padding: .06rem;
 	border-top: 1px solid white;
 	border-bottom: 1px solid white;
+}
+
+.recommend{
+  margin: 0 .9rem;
+}
+
+.recommend .title{
+  font-size: .08rem;
+  text-align: left;
+  font-weight: bolder;
+  margin: .08rem 0;
+}
+
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 0.6rem;
+    margin: 0;
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: rgba(69,137,148,.9);
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: rgba(69,137,148,.9);
+  }
+
+  .card{
+  border-radius: 8px;
+  box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  overflow: hidden;
+  color: #000;
+  background-color: #fff;
+  position:relative;
+  margin-bottom: .1rem;
+}
+
+img{
+  width: 100%;
+  display: block;
+}
+
+.text{
+  text-align: left;
+  padding: .12rem .08rem .08rem .08rem;
+  font-size: .08rem;
+  color: #fff;
+  background: rgba(0,0,0,.2);
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  left: 0;
+}
+
+.text span{
+  font-size: .05rem;
 }
 
 </style>
